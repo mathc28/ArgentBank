33057903d9faaafdf399098bf1dd3c60f12018a6
mathc28 <cocumathieu@gmail.com>
2024-09-03 18:18:22 +0200

    ajout firstname &  lastname dans la page user

--- src/components/greetings/index.jsx
+++ src/components/greetings/index.jsx
@@ -1,84 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import './style.css';
-import { setUserInfos } from '../../feature/login';
 
 const Greeting = () => {
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+  const { firstName, lastName } = useSelector((state) => state.login);
   const [display, setDisplay] = useState(false);
-  const [editedUser, setEditedUser] = useState({ username: user.username });
-  const [error, setError] = useState('');
-
-  // Effect to check token and fetch user info
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      fetch('http://localhost:3001/api/v1/user/profile', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('API Response:', data); // Debug
-          if (data.success) {
-            console.log('User data fetched:', data.body); // Debug
-            dispatch(setUserInfos(data.body)); // Assurez-vous que setUserInfos prend un objet utilisateur complet
-          } else {
-            setError('Failed to fetch user info');
-          }
-        })
-        .catch((error) => {
-        });
-    }
-  }, [dispatch]);
 
 
-  const handleChange = (event) => {
-    const { id, value } = event.target;
-    setEditedUser((prevState) => ({
-      ...prevState,
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const token = localStorage.getItem('token');
-    fetch('http://localhost:3001/api/v1/user/profile', {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(editedUser),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Update API Response:', data); // Debug
-        if (data.success) {
-          // Mettre à jour les informations dans Redux
-          dispatch(setUserInfos(editedUser));
-          // Masquer le formulaire
-          setDisplay(false);
-          setError('');
-        } else {
-          // Gérer les erreurs côté serveur
-          setError('Update error, please try again');
-        }
-      })
-      .catch((error) => {
-        console.error('Erreur :', error);
-        setError('Erreur lors de la mise à jour, veuillez réessayer.');
-      });
-  };
-
-  const handleCancel = () => {
-    setDisplay(false);
-  };
 
   return (
     <div className="background">
@@ -86,7 +14,7 @@ const Greeting = () => {
         <h1>
           Welcome back
           <br />
-          {user.firstname} {user.lastname}!
+          {firstName} {lastName}!
         </h1>
         {!display && (
           <button className="edit-button" onClick={() => setDisplay(true)}>
@@ -97,28 +25,27 @@ const Greeting = () => {
       {display && (
         <div className="hidden-form">
           <h2 className="hidden-form-title">Edit User Info</h2>
-          <form className="form" onSubmit={handleSubmit}>
+          <form className="form">
             <div className="edit-input">
               <label htmlFor="username">User name:</label>
-              <input type="text" id="username" value={editedUser.username} onChange={handleChange} />
+              <input type="text" id="username" value=""  /> 
             </div>
             <div className="edit-input">
               <label htmlFor="firstname">First name:</label>
-              <input type="text" id="firstname" value={user.firstname} readOnly />
+              <input type="text" id="firstname" value={firstName} readOnly />
             </div>
             <div className="edit-input">
               <label htmlFor="lastname">Last name:</label>
-              <input type="text" id="lastname" value={user.lastname} readOnly />
+              <input type="text" id="lastname" value={lastName} readOnly />
             </div>
             <div className="buttons">
               <button type="submit" className="edit-username-button">
                 Save
               </button>
-              <button type="button" className="edit-username-button" onClick={handleCancel}>
+              <button type="button" className="edit-username-button" >
                 Cancel
               </button>
             </div>
-            {error && <p className="error-message">{error}</p>}
           </form>
         </div>
       )}
